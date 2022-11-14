74bfd61ea351f23a4f1302ce2fe38de52a5c0522
Sand-jrd <47563242+Sand-jrd@users.noreply.github.com>
2022-11-14 11:20:49 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -15,13 +15,14 @@ Require the dependancy torch and kornia
 from GreeDS import GreeDS
 from vip_hci.fits import open_fits, write_fits
 
+dir = "your_directory"
 
-cube = open_fits("your_cube.fits")
-angles = open_fits("PA_angles.fits")
+cube = open_fits(dir+"your_cube.fits")
+angles = open_fits(dir+"your_PA_angles.fits")
 
 r = 10  # Iteration over PCA-rank
 l = 20  # Iteration per rank
 full_output = False  # If True, return every iterations. Better if you are searching optimized param
 
 res = GreeDS(cube, angles, r=r, l=l, full_output=True)
-write_fits("GreeDS_estimation", res)
+write_fits(dir+"GreeDS_estimation_"+str(r)+"_"+str(l), res)
